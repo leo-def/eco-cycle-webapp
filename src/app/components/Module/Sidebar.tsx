4c6ff12ddd5ded45a7dc9411e91a5dc5e5c96a3f
Leonardo de Freitas Oliveira <leo_defoliveira@live.com>
2023-01-01 12:06:37 -0300

    Sidebar: hide on click away

--- src/app/components/Module/Sidebar.tsx
+++ src/app/components/Module/Sidebar.tsx
@@ -1,7 +1,11 @@
 
 import React from 'react';
-import Drawer from '@mui/material/Drawer';
-import { Box, List } from '@mui/material';
+import {
+  Box,
+  List,
+  Drawer,
+  ClickAwayListener
+} from '@mui/material';
 import { getToggleSidebar, isSidebarOpen } from '../../hooks/Sidebar';
 import { getSetGroup } from '../../hooks/Module/Group/Set';
 import { GroupSidebar } from './Group/GroupSidebar';
@@ -17,13 +21,22 @@ export const Sidebar = React.memo((props: any) => {
   Sidebar.displayName = 'Sidebar';
   const anchor = 'left';
   const isOpen = isSidebarOpen();
+
   const toggleSidebar = getToggleSidebar();
   const setGroup = getSetGroup();
+
+  const handleClickAway = () => {
+    if(isOpen) {
+      toggleSidebar();
+    }
+  }
+
   return (
     <Drawer
       anchor={anchor}
       open={isOpen}
     >
+    <ClickAwayListener onClickAway={handleClickAway}>
       <Box
         sx={{ width: 250 }}
         role="presentation"
@@ -40,7 +53,7 @@ export const Sidebar = React.memo((props: any) => {
 
         </List>
       </Box>
-
+  </ClickAwayListener>
     </Drawer>
   );
 })
