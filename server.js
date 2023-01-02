30b3e333b7016e57196b58a93825c328defae359
Leonardo de Freitas Oliveira <leo_defoliveira@live.com>
2023-01-02 02:38:17 -0300

    Deploy: Add start command for prod

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(parseInt(process.env.PORT, 10) || 8080);
